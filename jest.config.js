08576bd9e6b462eb19b15155427d634e9e16eac9
guifavere <guilherme.favere@gmail.com>
2021-05-24 20:27:09 -0300

    Add jest config

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,12 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   "roots": [
     "<rootDir>/src"
-  ]
+  ],
+  moduleNameMapper: {
+    '^@adapter/(.*)$':  '<rootDir>/src/adapter/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@core/(.*)$':  '<rootDir>/src/core/$1',
+    '^@domain/(.*)$':  '<rootDir>/src/domain/$1',
+    '^@infra/(.*)$':  '<rootDir>/src/infra/$1'
+  }
 };
